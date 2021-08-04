a05186a3e00daab3c902fc30b2d6ff6783e25913
Chan Ngo <ngohongchan12a4@gmail.com>
2021-08-04 19:21:12 +0700

    clean architecture

---
+++ src/components/BoardBar/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.scss';
+
+const BoardBar = () => {
+  return <nav className='navbar-board'>Board Bar</nav>;
+};
+
+export default BoardBar;
---
+++ src/components/Task/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type Props = {};
+
+const Task: React.FC<Props> = () => {
+  return (
+    <li>
+      <img
+        src='https://trungquandev.com/wp-content/uploads/2021/05/trungquandev-cover-animation-1024x758.jpg'
+        alt='trello-background'
+      />
+      Titile: CHANNGO
+    </li>
+  );
+};
+
+export default Task;
