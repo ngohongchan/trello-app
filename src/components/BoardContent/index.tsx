a50ec5441258a23bf5f8eba3a4005abe0474665a
Chan Ngo <ngohongchan12a4@gmail.com>
2021-08-05 12:08:51 +0700

    initial_sample_data

--- src/components/BoardContent/index.tsx
+++ src/components/BoardContent/index.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import Columns from '../Columns';
+import { isEmpty } from 'lodash';
 import './styles.scss';
 
-const BoardContent = () => {
+import { initialData } from '../../actions/initialData';
+import { mapOrder } from '../../utilities/sort';
+
+type Props = {};
+
+const BoardContent: React.FC<Props> = () => {
+  const [board, setBoard] = React.useState<any>({});
+  const [columns, setColumns] = React.useState<any>([]);
+
+  React.useEffect(() => {
+    const boardFromDb = initialData.boards.find((boa) => boa.id === 'board-1');
+    if (boardFromDb) {
+      setBoard(boardFromDb);
+
+      setColumns(mapOrder(boardFromDb.columns, boardFromDb.columnOrder, 'id'));
+    }
+  }, [board]);
+
+  if (isEmpty(board)) {
+    return <div className='board-not-found'>Board not found</div>;
+  }
+
   return (
     <div className='board-columns'>
-      <Columns />
-      <Columns />
-      <Columns />
-      <Columns />
-      <Columns />
-      <Columns />
-      <Columns />
+      {columns.map((column: { title: string; cards: []; cardOrder: [] }, index: number) => (
+        <Columns key={index} column={column} />
+      ))}
     </div>
   );
 };
